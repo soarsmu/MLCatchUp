7f435d04ee1ce2583b3c84b4a5f7b9720fab8deb
stefanusagus <stefanusagusharyono@gmail.com>
2020-06-12 19:13:41 +0800

    Add transformation rule to change api name only

--- Python_AST_Test/ast_transform_rule.py
+++ Python_AST_Test/ast_transform_rule.py
@@ -55,14 +55,14 @@ class DefaultParamValueTransformer(ast.NodeTransformer):
             listKeywordParam = getKeywordArguments(node)
             isKeywordExist = False
             for keyword in listKeywordParam:
-                print(ast.dump(keyword))
+                # print(ast.dump(keyword))
                 # print(keyword.arg)
                 if keyword.arg == self.parameterName:
                     isKeywordExist = True
             if not isKeywordExist:
                 # If keyword is not exist yet, it means that the old API use the default value which is changed
                 # in the new API. Therefore, we need to create a new node
-                print("Keyword not exist")
+                # print("Keyword not exist")
                 newParam = createKeywordParam(self.parameterName, self.oldDefaultValue)
                 listKeywordParam.append(newParam)
 
@@ -77,3 +77,24 @@ class DefaultParamValueTransformer(ast.NodeTransformer):
         # print(ast.dump(tree))
         print_code(tree)
 
+
+class ApiNameTransformer(ast.NodeTransformer):
+    functionName = ""
+    newApiName = ""
+
+    def __init__(self, fname, newname):
+        self.functionName = fname
+        self.newApiName = newname
+        super().__init__()
+
+    def visit_Call(self, node: Call):
+        nodeFuncName = getFunctionName(node)
+        if nodeFuncName == self.functionName:
+            # Function name is correct
+            return changeFunctionName(node, self.newApiName)
+        return node
+
+    def transform(self, tree):
+        # print_code(tree)
+        self.visit(tree)
+        print_code(tree)
