d08659955a7c96ec3d2cfff4c26292ec05ff2693
stefanusagus <stefanusagusharyono@gmail.com>
2020-06-12 16:17:43 +0800

    Add utility function, getFunctionName

--- Python_AST_Test/ast_utils.py
+++ Python_AST_Test/ast_utils.py
@@ -3,3 +3,27 @@ import ast
 from ast import *
 def print_code(node):
     print(astunparse.unparse(node))
+
+# helper function to get the API invocation or function name from the node
+# e.g.
+# getFunctionName("sklearn.dummy.DummyClassifier") should return "DummyClassifier"
+# getFunctionName("DummyClassifier") should also return "Dummy Classifier"
+def getFunctionName(node):
+    print("\n")
+    print("\n")
+    print_code(node)
+    print(ast.dump(node))
+    try:
+        return node.func.id
+    except:
+        return node.func.attr
+
+# helper function to get the API invocation or function scope
+# e.g.
+# getFunctionScope("sklearn.dummy.DummyClassifier") should return "sklearn.dummy"
+def getFunctionScope(node):
+
+
+
+def getFunctionArguments(node):
+    return node
