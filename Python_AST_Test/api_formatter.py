6f10a54f3a2621751a8c88129fa4ee828cc4c9e4
stefanusagus <stefanusagusharyono@gmail.com>
2020-06-27 16:11:10 +0800

    Add separator between api name and api keyword

--- Python_AST_Test/api_formatter.py
+++ Python_AST_Test/api_formatter.py
@@ -7,12 +7,17 @@ def separate_api_parameter(node):
     # Loop through the given node from assignment to get all the API Call and keyword
     print(unparse(node))
     api_name = getFunctionName(node)
+    api_keywords = []
+    api_keywords.extend(getKeywordArguments(node))
+
     scope_list = recurseScope(node)
     for scope in scope_list:
         if scope is not None:
-            print(ast.dump(scope))
+            api_keywords.extend(getKeywordArguments(scope))
             api_name = getName(scope) + "." + api_name
     print("Full API Name: " + api_name)
+    print("FULL API KEYWORD: " + api_keywords.__str__())
+    api_keywords = getKeywordArguments(node)
     # print(ast.dump(node))
 
 class AssignmentVisitor(ast.NodeVisitor):
