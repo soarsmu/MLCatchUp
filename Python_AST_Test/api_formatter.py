bf3c18929161ef7e0e5e473f17b054bd617e1f0b
stefanusagus <stefanusagusharyono@gmail.com>
2020-06-28 02:42:57 +0800

    finish from import visitor

--- Python_AST_Test/api_formatter.py
+++ Python_AST_Test/api_formatter.py
@@ -3,24 +3,6 @@ from astunparse import unparse
 from ast import Assign
 from Python_AST_Test.ast_utils import *
 
-def separate_api_parameter(node):
-    # Loop through the given node from assignment to get all the API Call and keyword
-    api_name = getName(node)
-    api_keywords = []
-    api_keywords.extend(getKeywordArguments(node))
-    scope_list = recurseScope(node)
-    for scope in scope_list:
-        if scope is not None:
-            api_keywords.extend(getKeywordArguments(scope))
-            api_name = getName(scope) + "." + api_name
-    separated_signature = api_name + "#"
-    last_element = api_keywords[:-1]
-    for key in api_keywords:
-        separated_signature += key
-        if key != last_element:
-            separated_signature += ", "
-    return api_name, api_keywords
-
 class AssignmentVisitor(ast.NodeVisitor):
     def __init__(self):
         self.assignment_dictionary = {}
@@ -64,30 +46,51 @@ class FromImportVisitor(ast.NodeVisitor):
         self.from_import_dict = {}
 
     def visit_ImportFrom(self, node: ImportFrom):
-
         module_name = node.module
         imported_name = node.names[0].name
         imported_alias = node.names[0].asname
-        print("Module name: " + module_name)
-        print("Imported name: " + imported_name)
-        print("Imported alias: " + imported_alias.__str__())
         full_path = module_name + "." + imported_name
-        print("Full name: " + full_path)
         if imported_alias is not None:
-            print()
+            self.from_import_dict[imported_alias] = full_path
         else:
-            print()
+            self.from_import_dict[imported_name] = full_path
 
+class ApiFormatterVisitor(ast.NodeTransformer):
+    def __init__(self, import_dict, from_import_dict, assign_dict):
+        self.imp_dict = import_dict
+        self.from_dict = from_import_dict
+        self.ass_dict = assign_dict
+
+def separate_api_parameter(node):
+    # Loop through the given node from assignment to get all the API Call and keyword
+    api_name = getName(node)
+    api_keywords = []
+    api_keywords.extend(getKeywordArguments(node))
+    scope_list = recurseScope(node)
+    for scope in scope_list:
+        if scope is not None:
+            api_keywords.extend(getKeywordArguments(scope))
+            api_name = getName(scope) + "." + api_name
+    separated_signature = api_name + "#"
+    last_element = api_keywords[:-1]
+    for key in api_keywords:
+        separated_signature += key
+        if key != last_element:
+            separated_signature += ", "
+    return api_name, api_keywords
 
 # Tree is the AST of the complete file
 def process_api_format(tree):
-    # assignmentVisitor = AssignmentVisitor()
-    # assignmentVisitor.visit(tree)
-    # assign_dict = assignmentVisitor.assignment_dictionary
-    # print(assign_dict.__str__())
+    assignmentVisitor = AssignmentVisitor()
+    assignmentVisitor.visit(tree)
 
     importVisitor = ImportVisitor()
     importVisitor.visit(tree)
 
     fromImportVisitor = FromImportVisitor()
     fromImportVisitor.visit(tree)
+
+    assign_dict = assignmentVisitor.assignment_dictionary
+    import_dict = importVisitor.import_dictionary
+    from_import_dict = fromImportVisitor.from_import_dict
+
